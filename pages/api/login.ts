c6ec21178a365bfc7a1cce6711cade3e4d904898
Magno <magnop64@gmail.com>
2023-04-13 22:30:46 -0300

    inicio projeto devagran nextjs

---
+++ pages/api/login.ts
@@ -0,0 +1,20 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+
+const login = (req: NextApiRequest, res: NextApiResponse) => {
+    try{
+        if(req.method == 'POST'){
+            const {login, senha} = req.body;
+            if(login == 'admin@admin.com' &&
+               senha == 'senha123'){
+                return res.status(200).json({msg: 'Usuario logado com sucesso'})
+            }
+            return res.status(401).json({msg: 'Usuario ou senha informados não é valido...'});
+        }
+        return res.status(400).json({erro: 'Algo deu errado...'});
+    }catch(e){
+        console.log(e)
+        return res.status(500).json({erro: 'ops algo deu errado!'});
+    }
+}
+
+export default login;
